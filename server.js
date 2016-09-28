46aee0c03734055e7566b896e19a8ca65d3678f2
Andrej Urankar <andrejura@gmail.com>
2016-09-28 15:33:52 +0000

    Added dependencies in package.json.

--- server.js
+++ server.js
@@ -5,36 +5,46 @@ var fs = require('fs');
 var express = require('express');
 var app = express();
 var path = require('path');
+var passport = require('passport');
+var bodyParser = require('body-parser');
+var crypto = require('crypto');
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('./database.sqlite3');
+
+db.serialize(function() {
+    db.run("CREATE TABLE IF NOT EXISTS users (field INT, info TEXT)");
+    var stmt = db.prepare("INSERT into users values(?,?)");
+    app.post("/reg", function(req, res) {
+        console.log(req.body);
+    });
+    stmt.finalize;
+    
+    db.each("SELECT id,dt FROM users", function(err, row) {
+        if (err) {
+            throw err;
+        }
+        console.log("User id:" + row.id, row.dt);  
+    });
+});
+
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static(path.join(__dirname, '/public')));
 
-app.get("/login", function(req, res) {
-    if (checkUserData(req.param("username"), req.param("password"))) {
-    res.send("deluje?");
-    }
+app.get("/signup", function(req, res) {
+    res.sendFile(__dirname + "/public/register.html");
 });
 
-app.get('*', function(req, res) {
-    res.send('Not yet available.');
+app.post("/login", function(req, res) {
+    console.log(req.body);
 });
+/*app.get('*', function(req, res) {
+    res.send('Not yet available.');
+});*/
 
 app.listen(port);
 
 // Console will print the message
 console.log('Server running on port ' + port);
 
-var userDataServ = [{"u": "Andrej", "p": "123456"}];
-
-function checkUserData(username, password) {
-    /*fs.readFileSync("public/userData.json", "utf-8", function(err, data) {
-        userDataServ = JSON.parse("data");
-    });*/
-    console.log(userDataServ);
-    for (var i = 0; i < userDataServ.length; i++) {
-        if (username == userDataServ[i].u && password == userDataServ[i].p) {
-            console.log("works");
-            return true;
-        }
-    }
-    return false;
-}
